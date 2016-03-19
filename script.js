fd4006f7274e52ecd528ce09a70461c39260c18c
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-03-19 21:08:25 +0100

    loading gif added

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function generate(){
 	query_text = $("#query").val();
+	$("#results").hide();
+	$("#loading").show();
 	$.ajax({
 	  method: "POST",
 	  url: "http://127.0.0.1:5000/generate",
 	  data: { query: query_text}
 	})
 	  .done(function( msg ) {
+	  	$("#results").show();
 	  	$("#result").val(msg);
+	  	$("#loading").hide();
 	});
-
-
 }
 
 function reset(){
